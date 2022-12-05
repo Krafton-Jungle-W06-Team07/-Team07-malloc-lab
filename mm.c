a4b0fc9da70ce151dd4095a6c5540e16fe82c03e
ljy4201 <ljy4201@naver.com>
2022-12-05 20:23:40 +0900

    안돌아감

--- mm.c
+++ mm.c
@@ -1,6 +1,6 @@
 /*
  * mm-naive.c - The fastest, least memory-efficient malloc package.
- * 
+ *
  * In this naive approach, a block is allocated by simply incrementing
  * the brk pointer.  A block is pure payload. There are no headers or
  * footers.  Blocks are never coalesced or reused. Realloc is
@@ -21,17 +21,17 @@
 
 #define WSIZE 4
 #define DSIZE 8
-#define CHUNKSIZE (1<<12)
-#define MAX(x,y) ((x)>(y)? (x):(y))
+#define CHUNKSIZE (1 << 12)
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
 #define PACK(size, alloc) ((size) | (alloc))
 #define GET(p) (*(unsigned int *)(p))
 #define PUT(p, val) (*(unsigned int *)(p) = (val))
 #define GET_SIZE(p) (GET(p) & ~0x7)
 #define GET_ALLOC(p) (GET(p) & 0x1)
-#define HDRP(bp) ((char *)(bp) - WSIZE)
+#define HDRP(bp) ((char *)(bp)-WSIZE)
 #define FTRP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE)
-#define NEXT_BLKP(bp) ((char *)(bp) + GET_SIZE(((char *)(bp) - WSIZE)))
-#define PREV_BLKP(bp) ((char *)(bp) - GET_SIZE(((char *)(bp) - DSIZE)))
+#define NEXT_BLKP(bp) ((char *)(bp) + GET_SIZE(((char *)(bp)-WSIZE)))
+#define PREV_BLKP(bp) ((char *)(bp)-GET_SIZE(((char *)(bp)-DSIZE)))
 
 /*********************************************************
  * NOTE TO STUDENTS: Before you do anything else, please
@@ -47,109 +47,137 @@ team_t team = {
     /* Second member's full name (leave blank if none) */
     "",
     /* Second member's email address (leave blank if none) */
-    ""
-};
+    ""};
 
 /* single word (4) or double word (8) alignment */
 #define ALIGNMENT 8
 
 /* rounds up to the nearest multiple of ALIGNMENT */
-#define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~0x7)
-
+#define ALIGN(size) (((size) + (ALIGNMENT - 1)) & ~0x7)
 
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
-static char* heap_listp;
-/* 
+static char *heap_listp;
+/*
  * mm_init - initialize the malloc package.
  */
-static void *coalesce(void * bp){
-    void * pre_hd = HDRP(PREV_BLKP(bp));
-    void * next_hd = HDRP(NEXT_BLKP(bp));
-    if (GET_ALLOC(pre_hd) && GET_ALLOC(next_hd)) return bp; //prev랑 next 모두 할당되어 있을 때
-    if (!GET_ALLOC(pre_hd)) {
-        if (!GET_ALLOC(next_hd)){ // prev랑 next 가용블록일 때
-        PUT(HDRP(PREV_BLKP(bp)), PACK(GET_SIZE(HDRP(bp))+GET_SIZE(pre_hd)+GET_SIZE(next_hd), 0));
-        PUT(FTRP(PREV_BLKP(bp)), PACK(GET_SIZE(HDRP(bp))+GET_SIZE(pre_hd)+GET_SIZE(next_hd), 0));
-        }
-        else{ // prev만 가용 블록일 때
-        PUT(HDRP(PREV_BLKP(bp)) , PACK(GET_SIZE(HDRP(bp))+GET_SIZE(pre_hd), 0));
-        PUT(FTRP(PREV_BLKP(bp)) , PACK(GET_SIZE(HDRP(bp))+GET_SIZE(pre_hd), 0));
-        }
+static void *coalesce(void *bp)
+{
+    size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
+    size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
+    size_t size = GET_SIZE(HDRP(bp));
+    if (prev_alloc && next_alloc)
+    { /* Case 1 */
+        return bp;
+    }
+    else if (prev_alloc && !next_alloc)
+    { /* Case 2 */
+        size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+        PUT(HDRP(bp), PACK(size, 0));
+        PUT(FTRP(bp), PACK(size, 0));
+    }
+    else if (!prev_alloc && next_alloc)
+    { /* Case 3 */
+        size += GET_SIZE(HDRP(PREV_BLKP(bp)));
+        PUT(FTRP(bp), PACK(size, 0));
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
         bp = PREV_BLKP(bp);
     }
-    else if(!GET_ALLOC(next_hd)){ // next만 가용 블록일 때
-        PUT(HDRP(bp) , PACK(GET_SIZE(HDRP(bp))+GET_SIZE(next_hd), 0));
-        PUT(FTRP(bp) , PACK(GET_SIZE(HDRP(bp))+GET_SIZE(next_hd), 0));
+    else
+    { /* Case 4 */
+        size += GET_SIZE(HDRP(PREV_BLKP(bp))) + GET_SIZE(FTRP(NEXT_BLKP(bp)));
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+        PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
+        bp = PREV_BLKP(bp);
     }
     return bp;
 }
-static void * extend_heap(size_t words){
-    size_t r_words = ceil(words / DSIZE / WSIZE); //words를 2word의 배수로 반올림
-    void* bp = mem_sbrk(r_words * WSIZE); //추가적인 힙 공간 요청
-    if (bp == (void *)-1) return NULL; //요청 실패
-    PUT(HDRP(bp), PACK(r_words, 0)); //새 가용 블록 헤더
-    PUT(FTRP(bp), PACK(r_words, 0)); //새 가용 블록 풋터
-    PUT(HDRP(NEXT_BLKP(bp)), PACK(0,1)); //새 에필로그 블록
-
+static void *extend_heap(size_t words)
+{
+    char *bp;
+    size_t size;
+    size = (words % 2) ? (words + 1) * WSIZE : words * WSIZE;
+    if ((long)(bp = mem_sbrk(size)) == -1)
+        return NULL;
+    PUT(HDRP(bp), PACK(size, 0));
+    PUT(FTRP(bp), PACK(size, 0));
+    PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1));
     return coalesce(bp);
 }
 int mm_init(void)
 {
-    heap_listp = mem_sbrk(4*WSIZE);
-    if (heap_listp == (void*)-1) return -1;
-    PUT(heap_listp, 0); //미사용 패딩 워드
-    PUT(heap_listp + WSIZE, PACK(DSIZE, 1)); //prologue 블록 헤더
-    PUT(heap_listp + 2*WSIZE, PACK(DSIZE, 1)); //prologue 블록 풋터
-    PUT(heap_listp + 3*WSIZE, PACK(0, 1)); //epilogue 블록 헤더
-    heap_listp += 2*WSIZE; 
-
-    if (extend_heap(CHUNKSIZE/WSIZE) == NULL) return -1;
+    if ((heap_listp = mem_sbrk(4 * WSIZE)) == (void *)-1)
+        return -1;
+    PUT(heap_listp, 0);
+    PUT(heap_listp + (1 * WSIZE), PACK(DSIZE, 1));
+    PUT(heap_listp + (2 * WSIZE), PACK(DSIZE, 1));
+    PUT(heap_listp + (3 * WSIZE), PACK(0, 1));
+    heap_listp += (2 * WSIZE);
+    /* Alignment padding */
+    /* Prologue header */
+    /* Prologue footer */
+    /* Epilogue header */
+    /* Extend the empty heap with a free block of CHUNKSIZE bytes */
+    if (extend_heap(CHUNKSIZE / WSIZE) == NULL)
+        return -1;
     return 0;
 }
 
-/* 
+/*
  * mm_malloc - Allocate a block by incrementing the brk pointer.
  *     Always allocate a block whose size is a multiple of the alignment.
  */
 static void *find_fit(size_t asize)
 {
-    void* bp;
-    for (bp = heap_listp; GET_SIZE(bp)>0; bp = NEXT_BLKP(bp))
+    void *bp;
+    for (bp = heap_listp; GET_SIZE(bp) > 0; bp = NEXT_BLKP(bp))
     {
-        if (GET_SIZE(bp)>= asize && GET_ALLOC(bp) == 0) return bp;
+        if (GET_SIZE(bp) >= asize && GET_ALLOC(bp) == 0)
+            return bp;
     }
     return NULL;
 }
 static void place(void *bp, size_t asize)
 {
     size_t csize = GET_SIZE(HDRP(bp)) - asize;
-    if((csize >= 2 * DSIZE)){
+    if ((csize >= 2 * DSIZE))
+    {
         PUT(HDRP(bp), PACK(asize, 1));
         PUT(FTRP(bp), PACK(asize, 1));
         bp = NEXT_BLKP(bp);
         PUT(HDRP(bp), PACK(csize, 0));
         PUT(FTRP(bp), PACK(csize, 0));
     }
-    else{
+    else
+    {
         PUT(HDRP(bp), PACK(GET_SIZE(HDRP(bp)), 0));
         PUT(FTRP(bp), PACK(GET_SIZE(HDRP(bp)), 0));
     }
 }
 void *mm_malloc(size_t size)
 {
-    int newsize = ALIGN(size + SIZE_T_SIZE);
-    void* bp  = find_fit(newsize);
-    if (bp != NULL)
+    size_t asize;      /* Adjusted block size */
+    size_t extendsize; /* Amount to extend heap if no fit */
+    char *bp;
+    /* Ignore spurious requests */
+    if (size == 0)
+        return NULL;
+    /* Adjust block size to include overhead and alignment reqs. */
+    if (size <= DSIZE)
+        asize = 2 * DSIZE;
+    else
+    asize = DSIZE * ((size + (DSIZE) + (DSIZE-1)) / DSIZE);
+    /* Search the free list for a fit */
+    if ((bp = find_fit(asize)) != NULL)
     {
-        place(bp, newsize);
+        place(bp, asize);
         return bp;
     }
-    else{
-        bp = extend_heap(newsize/WSIZE);
-        if ( bp == NULL) return NULL;
-        place(bp, newsize);
-        return bp;
-    }
+    /* No fit found. Get more memory and place the block */
+    extendsize = MAX(asize, CHUNKSIZE);
+    if ((bp = extend_heap(extendsize / WSIZE)) == NULL)
+        return NULL;
+    place(bp, asize);
+    return bp;
 }
 /*
  * mm_free - Freeing a block does nothing.
@@ -170,28 +198,14 @@ void *mm_realloc(void *ptr, size_t size)
     void *oldptr = ptr;
     void *newptr;
     size_t copySize;
-    
+
     newptr = mm_malloc(size);
     if (newptr == NULL)
-      return NULL;
+        return NULL;
     copySize = *(size_t *)((char *)oldptr - SIZE_T_SIZE);
     if (size < copySize)
-      copySize = size;
+        copySize = size;
     memcpy(newptr, oldptr, copySize);
     mm_free(oldptr);
     return newptr;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
